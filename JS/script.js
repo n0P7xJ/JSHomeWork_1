13a5eab13cf7379dde78499cf69f2a3fca26f7f8
n0P7xJ <124531921+n0P7xJ@users.noreply.github.com>
2025-03-16 19:29:16 +0200

    First Homework JS

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+﻿
+fetch('../news.json')
+    .then(response => response.json())
+    .then(data => {
+        const newsContainer = document.getElementById('newsContainer');
+        data.forEach(news => {
+            const newsCard = `
+                <div class="card">
+                    <img src="${news.image}" class="card-img-top" alt="${news.title}">
+                    <div class="card-body">
+                        <h5 class="card-title">${news.title}</h5>
+                        <p class="card-text">${news.description}</p>
+                    </div>
+                </div>
+            `;
+            newsContainer.innerHTML += newsCard;
+        });
+    })
+    .catch(error => console.error('Error loading news:', error));
